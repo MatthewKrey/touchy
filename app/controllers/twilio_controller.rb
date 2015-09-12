17f89ae4c79374fec48cb30cfdeed621d823205e
GabiOC <oconnor.gabriele@gmail.com>
2015-09-12 16:08:17 -0400

    Add webhook module

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,27 +1,47 @@
-require 'rubygems' # not necessary with ruby 1.9 but included for completeness
+# require 'rubygems' # not necessary with ruby 1.9 but included for completeness
+# require 'twilio-ruby'
+#
+# class TwilioController < ApplicationController
+#   # credentials
+#   account_sid = Rails.application.secrets.twilio_client_id
+#   auth_token = Rails.application.secrets.twilio_auth_token
+#   phone_number = Rails.application.secrets.twilio_phone_number
+#   test_number = Rails.application.secrets.twilio_test_number
+#
+#   # users
+#   touchy_bot = phone_number
+#   test_num = test_number
+#   users = { '+19178644825' => "Gabi O'Connor" }
+#
+#   @client = Twilio::REST::Client.new account_sid, auth_token
+#
+#   # Send message to user
+#   users.each do |user_phone, user_name|
+#     message = @client.account.messages.create(
+#   	   :from => test_num,
+#        :to => user_phone,
+#        :body => "Hi #{user_name}, this is a test."
+#      )
+#      puts "Sent text to #{user_name}"
+#   end
+# end
+
+
 require 'twilio-ruby'
 
 class TwilioController < ApplicationController
-  # credentials
-  account_sid = Rails.application.secrets.twilio_client_id
-  auth_token = Rails.application.secrets.twilio_auth_token
-  phone_number = Rails.application.secrets.twilio_phone_number
-  test_number = Rails.application.secrets.twilio_test_number
+  include Webhookable
 
-  # users
-  touchy_bot = phone_number
-  test_num = test_number
-  users = { '+19178644825' => "Gabi O'Connor" }
+  after_filter :set_header
 
-  @client = Twilio::REST::Client.new account_sid, auth_token
+  skip_before_action :verify_authenticity_token
 
-  # Send message to user
-  users.each do |user_phone, user_name|
-    message = @client.account.messages.create(
-  	   :from => test_num,
-       :to => user_phone,
-       :body => "Hi #{user_name}, this is a test."
-     )
-     puts "Sent text to #{user_name}"
+  def voice
+  	response = Twilio::TwiML::Response.new do |r|
+  	  r.Say 'Hey there. Congrats on integrating Twilio into your Rails 4 app.', :voice => 'alice'
+         r.Play 'http://linode.rabasa.com/cantina.mp3'
+  	end
+
+  	render_twiml response
   end
 end
