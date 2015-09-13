22e8e0a690987c00ca648543ad619864af4acdca
MatthewKrey <kreyenglish@gmail.com>
2015-09-13 01:07:39 -0400

    adding talk_to_comfort_me_bot method

--- comfort_me_script.rb
+++ comfort_me_script.rb
@@ -3,36 +3,39 @@ require 'pry'
 
 class ComfortMe
 
-COMFORT_FLAGS = %w{
-  boobs,touch,kill,me,symptoms,biopsy,weird,dying,parents,can't,
-  cannot,sleep,diagnosis,bad,worst,tomorrow,know,appointment,
-  ultrasound,pictures,reason,shit,fuck,fck,fking,fcking,
-  calcification,mom,dad,brother,sister,family,hereditary,
-  shopping,splurging,clear,clear my mind,mind heart,god,God,GOD
-  shock,grandma,grandmother,nanna,grandpa,grandfather,annoying,pain,
-  gift,life,regret,regrets,regretting,resent,resentment,resentful
-  passed,passed out,passed away,treatment,tx,treatments,plans,
-  plan,planning,option,options,scared,scare,scary,MRI
-  test,tests,testing,IV,lose,losing,loss,hair,mutate,mutation,mutations,prevent,
-  prevents,prevention,morbid,cancer,cancerous,chemo,chemotherapy,echocardiogram,
-  tired,tiring,needles,blood,release,forgiveness,vulnerable, vulnerability,
-  sweetness,sweet,welcoming,welcome,emotional,emotion,emotionscontrol,controls,
-  controlling,body,own,ownership
-}
+  COMFORT_ME_BOT = ["i know this is a touchy subject", "i can help", "lets boogie down"]
 
-def initialize(message)
-  @message = message
-  #binding.pry
-end
+  COMFORT_FLAGS = %w{
+    boobs,touch,kill,me,symptoms,biopsy,weird,dying,parents,can't,
+    cannot,sleep,diagnosis,bad,worst,tomorrow,know,appointment,
+    ultrasound,pictures,reason,shit,fuck,fck,fking,fcking,
+    calcification,mom,dad,brother,sister,family,hereditary,
+    shopping,splurging,clear,clear my mind,mind heart,god,God,GOD
+    shock,grandma,grandmother,nanna,grandpa,grandfather,annoying,pain,
+    gift,life,regret,regrets,regretting,resent,resentment,resentful
+    passed,passed out,passed away,treatment,tx,treatments,plans,
+    plan,planning,option,options,scared,scare,scary,MRI
+    test,tests,testing,IV,lose,losing,loss,hair,mutate,mutation,mutations,prevent,
+    prevents,prevention,morbid,cancer,cancerous,chemo,chemotherapy,echocardiogram,
+    tired,tiring,needles,blood,release,forgiveness,vulnerable, vulnerability,
+    sweetness,sweet,welcoming,welcome,emotional,emotion,emotionscontrol,controls,
+    controlling,body,own,ownership
+  }
 
-def talk_to_comfort_me_bot
-  @message.each do |word|
-
+  def initialize(message)
+    @message = message
   end
-end
-  #
 
-
+  def talk_to_comfort_me_bot
+    @message.each do |word|
+      if @message.include?(COMFORT_FLAGS) == true
+        COMFORT_ME_BOT.sample
+      else
+        "whatchu want girl?"
+      end
+    end
+  end
+  
   # message.sample
 
 end
