72fe7baa5398290ccb92849482496833b6047b0c
stefania11 <druga.stefania@gmail.com>
2015-09-12 20:08:07 -0400

    removing tag user foreign key

--- db/migrate/20150912142647_create_tags.rb
+++ db/migrate/20150912142647_create_tags.rb
@@ -1,7 +1,6 @@
 class CreateTags < ActiveRecord::Migration
   def change
     create_table :tags do |t|
-      t.references :user, index: true, foreign_key: true
       t.references :message, index: true, foreign_key: true
     end
   end
